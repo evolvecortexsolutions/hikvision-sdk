cdfa7fc51bd6959cd780233ea635bde1178b5f6c
Aaditya Pandey <it@evolvecortex.com>
2026-03-19 05:04:16 +0530

    bug ../hikvision-sdk/internal/bridge/bridge.go:213:21: could not determine kind of name for C.BYTE

--- internal/bridge/bridge.go
+++ internal/bridge/bridge.go
@@ -5,88 +5,77 @@ package bridge
 /*
 #include <stdlib.h>
 
-typedef int BOOL;
-typedef unsigned short WORD;
-typedef int LONG;
-typedef unsigned int DWORD;
-typedef unsigned int UINT;
-typedef unsigned char BYTE;
-typedef void* LPVOID;
-typedef void* HWND;
-
 typedef struct {
     char sDVRIP[128];
-    WORD wDVRPort;
+    unsigned short wDVRPort;
     char sUserName[32];
     char sPassword[16];
-    DWORD dwBaudRate;
-    BYTE byDVRType;
-    BYTE byAlarmEnable;
-    WORD wDecoderDevicePort;
-    WORD byChannel;
-    BYTE byStartingChannel;
-    BYTE byAudioChannel;
-    BYTE byIPChannel;
-    BYTE byZeroChannel;
-    BYTE byMainProto;
-    BYTE bySubProto;
-    BYTE bySupport;
-    DWORD dwSupport;
+    unsigned int dwBaudRate;
+    unsigned char byDVRType;
+    unsigned char byAlarmEnable;
+    unsigned short wDecoderDevicePort;
+    unsigned short byChannel;
+    unsigned char byStartingChannel;
+    unsigned char byAudioChannel;
+    unsigned char byIPChannel;
+    unsigned char byZeroChannel;
+    unsigned char byMainProto;
+    unsigned char bySubProto;
+    unsigned char bySupport;
+    unsigned int dwSupport;
     char sSupportRet[16];
-    DWORD dwSerialNumber;
-    DWORD dwAlarmInPortNum;
-    DWORD dwAlarmOutPortNum;
-    DWORD dwDiskNum;
-    BYTE byDVRTypeEx;
-    BYTE bySupport2;
-    WORD byRes2;
-    DWORD dwStep;
-    DWORD dwChanNum;
-    BYTE byVoiceInChanNum;
-    BYTE byStartChan;
-    BYTE byRes[2];
+    unsigned int dwSerialNumber;
+    unsigned int dwAlarmInPortNum;
+    unsigned int dwAlarmOutPortNum;
+    unsigned int dwDiskNum;
+    unsigned char byDVRTypeEx;
+    unsigned char bySupport2;
+    unsigned short byRes2;
+    unsigned int dwStep;
+    unsigned int dwChanNum;
+    unsigned char byVoiceInChanNum;
+    unsigned char byStartChan;
+    unsigned char byRes[2];
 } NET_DVR_DEVICEINFO_V30;
 
 typedef struct {
-    DWORD dwYear;
-    DWORD dwMonth;
-    DWORD dwDay;
-    DWORD dwHour;
-    DWORD dwMinute;
-    DWORD dwSecond;
+    unsigned int dwYear;
+    unsigned int dwMonth;
+    unsigned int dwDay;
+    unsigned int dwHour;
+    unsigned int dwMinute;
+    unsigned int dwSecond;
 } NET_DVR_TIME;
 
 typedef struct {
-    DWORD dwSize;
+    unsigned int dwSize;
     void* struIDInfo;
     NET_DVR_TIME struBeginTime;
     NET_DVR_TIME struEndTime;
-    HWND hWnd;
-    BYTE byDrawFrame;
-    BYTE byVolumeType;
-    BYTE byVolumeNum;
-    BYTE byStreamType;
-    DWORD dwFileIndex;
-    BYTE byAudioFile;
-    BYTE byCourseFile;
-    BYTE byDownload;
-    BYTE byOptimalStreamType;
-    BYTE byUseAsyn;
-    BYTE byRes2[19];
+    void* hWnd;
+    unsigned char byDrawFrame;
+    unsigned char byVolumeType;
+    unsigned char byVolumeNum;
+    unsigned char byStreamType;
+    unsigned int dwFileIndex;
+    unsigned char byAudioFile;
+    unsigned char byCourseFile;
+    unsigned char byDownload;
+    unsigned char byOptimalStreamType;
+    unsigned char byUseAsyn;
+    unsigned char byRes2[19];
 } NET_DVR_VOD_PARA;
 
-extern BOOL NET_DVR_Init(void);
-extern BOOL NET_DVR_Cleanup(void);
-extern LONG NET_DVR_Login_V30(char *sDVRIP, WORD wDVRPort, char *sUserName, char *sPassword, NET_DVR_DEVICEINFO_V30 *lpDeviceInfo);
-extern BOOL NET_DVR_Logout(LONG lUserID);
-extern DWORD NET_DVR_GetLastError(void);
-extern LONG NET_DVR_StartVoiceCom(LONG lUserID, void(*)(LONG, char*, DWORD, BYTE, DWORD), DWORD dwUser);
-extern BOOL NET_DVR_StopVoiceCom(LONG lVoiceComHandle);
-extern LONG NET_DVR_PlayBackByTime_V40(LONG lUserID, const NET_DVR_VOD_PARA* pVodPara);
-extern BOOL NET_DVR_StopPlayBack(LONG lPlayHandle);
+extern int NET_DVR_Init(void);
+extern int NET_DVR_Cleanup(void);
+extern int NET_DVR_Login_V30(char *sDVRIP, unsigned short wDVRPort, char *sUserName, char *sPassword, NET_DVR_DEVICEINFO_V30 *lpDeviceInfo);
+extern int NET_DVR_Logout(int lUserID);
+extern unsigned int NET_DVR_GetLastError(void);
+extern int NET_DVR_StartVoiceCom(int lUserID, void(*callback)(int, char*, unsigned int, unsigned char, unsigned int), unsigned int dwUser);
+extern int NET_DVR_StopVoiceCom(int lVoiceComHandle);
+extern int NET_DVR_PlayBackByTime_V40(int lUserID, const NET_DVR_VOD_PARA* pVodPara);
+extern int NET_DVR_StopPlayBack(int lPlayHandle);
 */
-// import "C"
-
 import "C"
 
 import (
@@ -141,7 +130,7 @@ func LoginV30(ip string, port uint16, username string, password string) (int32, 
 	defer C.free(unsafe.Pointer(cPass))
 
 	var info C.NET_DVR_DEVICEINFO_V30
-	userID := C.NET_DVR_Login_V30(cIP, C.WORD(port), cUser, cPass, &info)
+	userID := C.NET_DVR_Login_V30(cIP, C.ushort(port), cUser, cPass, &info)
 	if userID == -1 {
 		errCode := C.NET_DVR_GetLastError()
 		return -1, fmt.Errorf("NET_DVR_Login_V30 failed with code %d", int(errCode))
@@ -154,7 +143,7 @@ func Logout(userID int32) error {
 	if userID < 0 {
 		return errors.New("invalid userID")
 	}
-	if C.NET_DVR_Logout(C.LONG(userID)) == 0 {
+	if C.NET_DVR_Logout(C.int(userID)) == 0 {
 		errCode := C.NET_DVR_GetLastError()
 		return fmt.Errorf("NET_DVR_Logout failed with code %d", int(errCode))
 	}
@@ -166,7 +155,7 @@ func StartTalk(userID int32) (int32, error) {
 	if !inited {
 		return -1, errors.New("SDK not initialized: call InitSDK first")
 	}
-	voiceHandle := C.NET_DVR_StartVoiceCom(C.LONG(userID), nil, 0)
+	voiceHandle := C.NET_DVR_StartVoiceCom(C.int(userID), nil, 0)
 	if voiceHandle == -1 {
 		errCode := C.NET_DVR_GetLastError()
 		return -1, fmt.Errorf("NET_DVR_StartVoiceCom failed with code %d", int(errCode))
@@ -179,7 +168,7 @@ func StopTalk(voiceHandle int32) error {
 	if voiceHandle < 0 {
 		return errors.New("invalid voice handle")
 	}
-	if C.NET_DVR_StopVoiceCom(C.LONG(voiceHandle)) == 0 {
+	if C.NET_DVR_StopVoiceCom(C.int(voiceHandle)) == 0 {
 		errCode := C.NET_DVR_GetLastError()
 		return fmt.Errorf("NET_DVR_StopVoiceCom failed with code %d", int(errCode))
 	}
@@ -193,32 +182,32 @@ func PlayBackByTime(userID int32, start time.Time, end time.Time, streamType uin
 	}
 
 	var vod C.NET_DVR_VOD_PARA
-	vod.dwSize = C.DWORD(unsafe.Sizeof(vod))
-	vod.struBeginTime.dwYear = C.DWORD(start.Year())
-	vod.struBeginTime.dwMonth = C.DWORD(start.Month())
-	vod.struBeginTime.dwDay = C.DWORD(start.Day())
-	vod.struBeginTime.dwHour = C.DWORD(start.Hour())
-	vod.struBeginTime.dwMinute = C.DWORD(start.Minute())
-	vod.struBeginTime.dwSecond = C.DWORD(start.Second())
-	vod.struEndTime.dwYear = C.DWORD(end.Year())
-	vod.struEndTime.dwMonth = C.DWORD(end.Month())
-	vod.struEndTime.dwDay = C.DWORD(end.Day())
-	vod.struEndTime.dwHour = C.DWORD(end.Hour())
-	vod.struEndTime.dwMinute = C.DWORD(end.Minute())
-	vod.struEndTime.dwSecond = C.DWORD(end.Second())
-	vod.hWnd = C.HWND(nil)
-	vod.byDrawFrame = 0
-	vod.byVolumeType = 0
-	vod.byVolumeNum = 0
-	vod.byStreamType = C.BYTE(streamType)
-	vod.dwFileIndex = C.DWORD(fileIndex)
-	vod.byAudioFile = 0
-	vod.byCourseFile = 0
-	vod.byDownload = 0
-	vod.byOptimalStreamType = 0
-	vod.byUseAsyn = 0
+	vod.dwSize = C.uint(unsafe.Sizeof(vod))
+	vod.struBeginTime.dwYear = C.uint(start.Year())
+	vod.struBeginTime.dwMonth = C.uint(start.Month())
+	vod.struBeginTime.dwDay = C.uint(start.Day())
+	vod.struBeginTime.dwHour = C.uint(start.Hour())
+	vod.struBeginTime.dwMinute = C.uint(start.Minute())
+	vod.struBeginTime.dwSecond = C.uint(start.Second())
+	vod.struEndTime.dwYear = C.uint(end.Year())
+	vod.struEndTime.dwMonth = C.uint(end.Month())
+	vod.struEndTime.dwDay = C.uint(end.Day())
+	vod.struEndTime.dwHour = C.uint(end.Hour())
+	vod.struEndTime.dwMinute = C.uint(end.Minute())
+	vod.struEndTime.dwSecond = C.uint(end.Second())
+	vod.hWnd = nil
+	vod.byDrawFrame = C.uchar(0)
+	vod.byVolumeType = C.uchar(0)
+	vod.byVolumeNum = C.uchar(0)
+	vod.byStreamType = C.uchar(streamType)
+	vod.dwFileIndex = C.uint(fileIndex)
+	vod.byAudioFile = C.uchar(0)
+	vod.byCourseFile = C.uchar(0)
+	vod.byDownload = C.uchar(0)
+	vod.byOptimalStreamType = C.uchar(0)
+	vod.byUseAsyn = C.uchar(0)
 
-	playHandle := C.NET_DVR_PlayBackByTime_V40(C.LONG(userID), &vod)
+	playHandle := C.NET_DVR_PlayBackByTime_V40(C.int(userID), &vod)
 	if playHandle == -1 {
 		errCode := C.NET_DVR_GetLastError()
 		return -1, fmt.Errorf("NET_DVR_PlayBackByTime_V40 failed with code %d", int(errCode))
@@ -231,7 +220,7 @@ func StopPlayback(playHandle int32) error {
 	if playHandle < 0 {
 		return errors.New("invalid playback handle")
 	}
-	if C.NET_DVR_StopPlayBack(C.LONG(playHandle)) == 0 {
+	if C.NET_DVR_StopPlayBack(C.int(playHandle)) == 0 {
 		errCode := C.NET_DVR_GetLastError()
 		return fmt.Errorf("NET_DVR_StopPlayBack failed with code %d", int(errCode))
 	}
